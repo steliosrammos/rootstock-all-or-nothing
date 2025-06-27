eb5c4b32cad038c439b1f67f8eb21e355d0e5b99
michael1011 <me@michael1011.at>
2025-06-27 19:58:48 +0200

    feat: simple cancel

--- src/Aon.sol
+++ src/Aon.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.30;
 
+interface IOwnable {
+    function owner() external view returns (address);
+}
+
 contract Aon {
     error GoalNotReached();
     error GoalReachedAlready();
@@ -10,7 +14,13 @@ contract Aon {
     address payable public immutable creator;
     uint256 public immutable goal;
 
+    IOwnable public immutable factory;
+
     mapping(address => uint256) public contributions;
+
+    constructor() {
+        factory = IOwnable(msg.sender);
+    }
 
     function claim() external {
         require(goalReached(), GoalNotReached());
@@ -24,6 +34,11 @@ contract Aon {
         require(msg.value > 0, InvalidContribution());
 
         contributions[msg.sender] += msg.value;
+    }
+
+    function cancel() external {
+        require(msg.sender == factory.owner(), "Only factory can cancel");
+        selfdestruct(payable(factory.owner()));
     }
 
     function goalReached() public view returns (bool) {
