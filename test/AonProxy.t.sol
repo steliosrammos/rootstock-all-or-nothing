e9692b0832cc54feb0b38834c054a0c4518980af
Stelios Rammos <stelio.rammos@gmail.com>
2025-10-30 09:35:21 -0300

    fix: remove unused var

--- test/AonProxy.t.sol
+++ test/AonProxy.t.sol
@@ -181,7 +181,6 @@ contract AonProxyTest is Test {
     function test_Proxy_WithZeroImplementation() public {
         // Test proxy with zero address implementation
         vm.expectRevert(AonProxy.InvalidImplementation.selector);
-        AonProxy zeroProxy = new AonProxy(address(0));
     }
 
     function test_Proxy_WithDifferentImplementations() public {
